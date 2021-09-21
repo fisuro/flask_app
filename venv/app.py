309317c3841afcc6d7f53b67bfd83842e8ed2db3
fisuro <gazdahusmir.ba@gmail.com>
2021-09-21 18:01:58 +0200

    update on app

--- venv/app.py
+++ venv/app.py
@@ -1,7 +1,40 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+users = [
+    { 
+        "id":0,
+        "name":"Filip",
+        "surname":"Bjelic"
+    },
+    {
+        "id":1,
+        "name":"Aleksandar",
+        "surname":"Vukovac"
+    }
+]
+
+@app.route("/api/v1/resources/users/all", methods=['GET'])
+def api_all():
+    return jsonify(users)
+
+@app.route("/api/v1/resources/users", methods=['GET'])
+def api_id():
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id provided."
+
+    results = []
+
+    for user in users:
+        if user['id'] == id:
+            results.append(user)
+
+    return jsonify(results)
+
+@app.route("/healtcheck", methods=['GET'])
+def healtcheck():
+    return 'Is healty'
