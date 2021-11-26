f385b6222ac31c53f96233e305a836de2498f7fa
fisuro <fisuro@gmail.com>
2021-11-26 17:37:04 +0100

    update on data structure, validation and separating methods

--- app.py
+++ app.py
@@ -13,67 +13,82 @@ app.config["DEBUG"] = True
 # run_with_ngrok(app)
 
 #Loading schema to a variable
-schema = json.load(open('schema.json'))
-
+def load_schema():
+    schema = json.load(open('schema.json'))
+    return schema
+#This function goes trough the keys(id's) inside users file and get's the last id, then parses it to int, adds a counter and returns the new ID as a string
+def get_index():
+    index = int((list(load_users().keys()))[-1]) + 1
+    return str(index)
+    
 #Reading users data from json file and storing them as variable
-json_object = json.load(open('users.json'))
+def load_users():
+    json_object = json.load(open('users.json'))
+    return json_object
 
+#This helper function is writing the the whole dict to a file
 def write_to_file(file, object):
     with open(file, 'w') as writefile:
         writefile.write(object)
 
 #Function for validation json data sent by POST request with catching exception
-#And iretates trough the existing users comparing the Id's returning only true if
+#And iretates trough the existing users comparing the emails returning only true if
 #A the JSON schema is right and there is no user with the same ID
 def validate_json(to_validate):
+    schema = load_schema()
     try:
         validate(instance=to_validate, schema=schema)
     except jsonschema.exceptions.ValidationError as err:
-        return False
+        return "Bad request", 400
 
+    json_object = load_users()
     for user in json_object:
-        if user['email'] == to_validate['email'] or user['id'] == to_validate['id']:
-            return False
-    return True
+        if json_object[user]['email'] == to_validate['email']:
+            return "Email already in use", 406
+    return "Success"
  
 #Defining route for reading and adding users 
-@app.route("/users", methods=['GET', 'POST'])
+@app.route("/users", methods=['GET'])
 def api_all():
-    if request.method == 'POST':
-        json_data = request.json
-        json_data["id"] = json_object[-1]['id'] + 1
-        #Calling a function to validate JSON, if JSON follows schema
-        #Then the data from request is stored in variable, and then saved to a file
-        if validate_json(json_data):
-            #If JSON form is valid, data from request is added to the list
-            json_object.append(json_data)
-            write_to_file('users.json', json.dumps(json_object))
-            return json_data
-        else: return "Bad request", 400
-    else: 
-        return jsonify(json_object)
+    return jsonify(load_users())
 
-@app.route("/users/<int:id>", methods=['GET', 'PUT', 'DELETE'])
-def api_id(id):
-    for user in json_object:
-        if user['id'] == id:
-            if request.method == 'DELETE':
-                json_object.remove(user)
-                write_to_file('users.json', json.dumps(json_object))
-                return 'Sucess', 203
-            elif request.method == 'PUT':
-                json_data = request.json
-                if json_data['name']:
-                    user['name'] = json_data['name']
-                if json_data['surname']:
-                    user['surname'] = json_data['surname']
-                to_write = json.dumps(json_object)
-                write_to_file('users.json', to_write)
-                return jsonify(user)
-                # return render_template('users.html')
-            else:    
-                # return jsonify(user)
-                return render_template('user.html')
+@app.route("/users", methods=['POST'])
+def add_user():
+    if(validate_json(request.json) == "Success"):
+        json_object = load_users()
+        json_object[get_index()] = request.json
+        write_to_file('users.json', json.dumps(json_object))
+        return json_object
+    else: return validate_json(request.json)
+
+@app.route("/users/<string:id>", methods=['GET'])
+def get_user(id):
+    json_object = load_users()
+    if id in json_object.keys():
+        # return jsonify(json_object[id])
+        return render_template('user.html')
+    else: return 'Error, non-correct id passed', 400
+
+@app.route("/users/<string:id>", methods=['DELETE'])
+def delete_user(id):
+    json_object = load_users()
+    if id in json_object.keys():
+        json_object.pop(id)
+        write_to_file('users.json', json.dumps(json_object))
+        return 'Success', 200
+    return 'Error, non-correct id passed', 400
+
+@app.route("/users/<string:id>", methods=['PUT'])
+def update_user(id):
+    json_object = load_users()
+    if id in json_object.keys():
+        json_object[id] = {
+            "name":request.json['name'],
+            "surname":request.json['surname'],
+            "email": json_object[id]['email']
+        }
+        write_to_file('users.json', json.dumps(json_object))
+        return json_object
     return 'Error, non-correct id passed', 400
 
 @app.route("/healtcheck", methods=['GET'])
